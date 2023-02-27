8b1aad5407e8e3f4a40bc33ecc082c8de6075b12
4Vitalii <4vo.ivanov@gmail.com>
2023-02-27 18:58:24 +0300

    my commit

--- src/Worker/main.java
+++ src/Worker/main.java
@@ -4,10 +4,10 @@ public class main {
     public static void main(String[] args) {
         GenerateId generateId = new GenerateId();
         PostMan postMan = new PostMan(32, "John",0, 350.000, "Почтальон" , 60);
-        System.out.println(generateId.generateId());
-        System.out.println(postMan.calculatePay());
+        System.out.println("Id сотрудника: " + generateId.generateId());
+        System.out.println("Зарплата + премия: " + postMan.calculatePay());
         SalesMan salesMan = new SalesMan(19, "Raymond", 0, 120.000, "Продавец",35);
-        System.out.println(generateId.generateId());
-        System.out.println(salesMan.calculatePay());
+        System.out.println("Id сотрудника: " + generateId.generateId());
+        System.out.println("Зарплата + премия: " + salesMan.calculatePay());
     }
 }
