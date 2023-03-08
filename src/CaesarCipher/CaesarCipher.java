5d95a51e6d383925d8e567719ce067cf5337b5c5
4Vitalii <4vo.ivanov@gmail.com>
2023-03-08 20:42:52 +0300

    my commit

--- src/CaesarCipher/CaesarCipher.java
+++ src/CaesarCipher/CaesarCipher.java
@@ -35,15 +35,44 @@ public class CaesarCipher {
             }
         }
 
-        String shiftAlphabet = alphabet.substring(0,alphabet.length() - 1);
+        String shiftAlphabet = alphabet.substring(0,alphabet.length() - 1); // делаем сдвиг на 1 в ->)
         shiftAlphabet = alphabet.charAt(alphabet.length() - 1) + shiftAlphabet;
 
         return shiftAlphabet;
     }
 
-    public char encode (char symbol) {
+    public char encode (char symbol) {  // Ищем в оригинальном алфавите символ и его индекс. Используем его в зашифрованном алфавите.
         int index = originalAlphabet.indexOf(symbol);
         return alphabet.charAt(index);
+    }
+
+    public String encryptMessage (String message) { // разбиваем переменную типа String на переменные типа char и с помощью цикла и метода encode собираем и возвращаем зашифрованное слово
+        char chEncode;
+        String encodeMessage = "";
+
+        for (int i = 0; i < message.length(); i++) {
+            chEncode = message.charAt(i);
+            encode(chEncode);
+            encodeMessage += encode(chEncode);
+        }
+        return encodeMessage;
+    }
+
+    public char decode (char symbol) {  // Ищем в зашифрованном алфавите символ и его индекс. Используем его в оригинальном алфавите.
+        int index = alphabet.indexOf(symbol);
+        return originalAlphabet.charAt(index);
+    }
+
+    public String decodeTheMessage (String message) { // Ищем в оригинальном алфавите символ и его индекс. Используем его в зашифрованном алфавите.
+        char chDecode;
+        String decodeMessage = "";
+
+        for (int i = 0; i < message.length(); i++) {
+            chDecode = message.charAt(i);
+            decode(chDecode);
+            decodeMessage += decode(chDecode);
+        }
+        return decodeMessage;
     }
 }
 
--- src/CaesarCipher/main.java
+++ src/CaesarCipher/main.java
@@ -4,5 +4,7 @@ public class main {
     public static void main(String[] args) {
         CaesarCipher caesarCipher = new CaesarCipher("мама", "абвгдеёжзийклмнопрстуфхцчшщъыьэюя");
         System.out.println(caesarCipher.buildAlphabet());
+        System.out.println(caesarCipher.encryptMessage("приветкатя"));
+        System.out.println(caesarCipher.decodeTheMessage("опжагсиясю"));
     }
 }
