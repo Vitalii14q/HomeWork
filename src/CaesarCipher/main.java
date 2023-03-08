5df4affaff539470f07330a7b1a9fffa47d0602b
4Vitalii <4vo.ivanov@gmail.com>
2023-03-08 20:45:22 +0300

    my commit

--- src/CaesarCipher/main.java
+++ src/CaesarCipher/main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) {
         CaesarCipher caesarCipher = new CaesarCipher("мама", "абвгдеёжзийклмнопрстуфхцчшщъыьэюя");
         System.out.println(caesarCipher.buildAlphabet());
-        System.out.println(caesarCipher.encryptMessage("приветкатя"));
-        System.out.println(caesarCipher.decodeTheMessage("опжагсиясю"));
+        System.out.println(caesarCipher.encryptMessage("привет"));
+        System.out.println(caesarCipher.decodeTheMessage("опжагс"));
     }
 }
